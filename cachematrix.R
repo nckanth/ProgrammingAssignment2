ab0910b5c2e575ca51dd746b8a7efa10dbc52cc3
nckanth <nckanth@gmail.com>
2016-01-24 02:58:19 +0000

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+#cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
+#Computing the inverse of a square matrix is done with the solve function in R. For example, if X is a square invertible matrix, then solve(X) returns its inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#this function caches inverse of matrix & its input matrix, it also has set get setInverse getInvese functions which operate on Inversematrix and its correspinding matrix
+makeCacheMatrix  <- function (cachedmatrix= matrix())
+  
+{
+  #check if input parameter is null
+  invcachedmatrix <- NULL
+  
+  set<- function(y)
+  {
+    cachedmatrix <<- y
+    invcachedmatrix  <<-NULL
+  }
+  
+  get<- function()
+  {
+    cachedmatrix 
+  }
+  
+  setInverse <- function (inversematrix)
+  {
+    
+    invcachedmatrix  <<- inversematrix
+    
+  }
+  
+  getInverse<- function ()
+  {
+    invcachedmatrix 
+  }
+  
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function checks if inverse of matrix exist , if not it will create one
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve  <-function(x,...)
+{
+  
+  inversematrix <- x$getInverse()
+  
+  if(!is.null(inversematrix ))
+  {
+    message("getting cached data")
+    return(inversematrix )
+  }
+  
+  
+  inversematrix  <- solve(x$get(), ...)
+  
+  x$setInverse(inversematrix )
+  
+  inversematrix 
+  
 }
